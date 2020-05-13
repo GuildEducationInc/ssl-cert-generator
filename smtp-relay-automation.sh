3295565ab4910608825e1e88e01d1f359d410f9e
Trey Tomlinson <trey.tomlinson@guildeducation.com>
2020-05-13 11:22:53 -0600

    finished automation piece

--- smtp-relay-automation.sh
+++ smtp-relay-automation.sh
@@ -3,16 +3,13 @@
 "$@"
 
 ACCOUNT="$(aws sts get-caller-identity | jq -r '.Account')"
-echo "${ACCOUNT}"
 
 if [[ $ACCOUNT -eq 947618278001 ]]
 then
-  echo "prod"
   DOMAIN="guild-cloud.com"
   ENVIRONMENT="prod"
 elif [[ $ACCOUNT -eq 477873552632 ]]
 then
-  echo "dev"
   DOMAIN="dev-guild-cloud.com"
   ENVIRONMENT="dev"
 else
@@ -23,12 +20,12 @@ aws ssm put-parameter \
   --region us-west-2 \
   --name "smtp-relay-cert-${ENVIRONMENT}" \
   --type "String" \
-  --value "$(cat acme.sh/smtp-relay-test.${DOMAIN}/smtp-relay-test.${DOMAIN}.cer)" \
+  --value "$(cat acme.sh/smtp-relay.${DOMAIN}/fullchain.cer)" \
   --overwrite
 
 aws ssm put-parameter \
   --region us-west-2 \
   --name "smtp-relay-key-${ENVIRONMENT}" \
   --type "String" \
-  --value "$(cat acme.sh/smtp-relay-test.${DOMAIN}/smtp-relay-test.${DOMAIN}.key)" \
+  --value "$(cat acme.sh/smtp-relay.${DOMAIN}/smtp-relay.${DOMAIN}.key)" \
   --overwrite
