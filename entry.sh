d89ae223a34bf70646748acde98efe15ad70583e
Trey Tomlinson <trey.tomlinson@guildeducation.com>
2020-06-04 09:30:21 -0600

    refactored smtp-relay-automation.sh / added error handling

--- entry.sh
+++ entry.sh
@@ -3,5 +3,5 @@ if [ "$1" = "daemon" ];  then
  trap "echo stop && killall crond && exit 0" SIGTERM SIGINT
  crond && while true; do sleep 1; done;
 else
- /smtp-relay-automation.sh "$@"
+ /smtp-relay-automation.sh
 fi
--- smtp-relay-automation.sh
+++ smtp-relay-automation.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env sh
 
-"$@"
-
 ACCOUNT="$(aws sts get-caller-identity | jq -r '.Account')"
 
 if [[ $ACCOUNT -eq 947618278001 ]]
 then
   DOMAIN="guild-cloud.com"
   ENVIRONMENT="prod"
+  /root/.acme.sh/acme.sh --issue --dns dns_aws -d smtp-relay.guild-cloud.com -d smtp-relay.guild-cloud.com
 elif [[ $ACCOUNT -eq 477873552632 ]]
 then
   DOMAIN="dev-guild-cloud.com"
   ENVIRONMENT="dev"
+   /root/.acme.sh/acme.sh --issue --dns dns_aws -d smtp-relay.dev-guild-cloud.com -d smtp-relay.dev-guild-cloud.com
 else
   echo "unable to identify AWS account"
+  exit 1
 fi
 
 aws ssm put-parameter \
